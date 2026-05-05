510c5a402be16e42039603ef9669f8eaeb8b6cb2
satya2330 <93843037+satya2330@users.noreply.github.com>
2026-05-05 13:17:51 -0500

    Update terraform.sh

--- scripts/terraform.sh
+++ scripts/terraform.sh
@@ -14,9 +14,9 @@ gpg --no-default-keyring \
 --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
 --fingerprint
 
+
 # Add HashiCorp repository to sources list
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | 
 sudo tee /etc/apt/sources.list.d/hashicorp.list
 
 # Update package lists
